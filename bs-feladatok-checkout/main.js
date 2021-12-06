134278d3118d7bb247e0e8ee119122a13d719373
vargapeter66 <varga.peter.mobil@gmail.com>
2021-12-06 11:08:10 +0100

    v1

---
+++ bs-feladatok-checkout/main.js
@@ -0,0 +1,16 @@
+const usStates = ['Choose...','Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+const hunCounties = ['Válassz...','Bács-Kiskun', 'Baranya', 'Békés', 'Borsod-Abaúj-Zemplén', 'Csongrád-Csanád', 'Fejér', 'Győr-Moson-Sopron', 'Hajdú-Bihar', 'Heves', 'Jász-Nagykun-Szolnok', 'Komárom-Esztergom', 'Nógrád', 'Pest', 'Somogy', 'Szabolcs-Szatmár-Bereg', 'Tolna', 'Vas', 'Veszprém', 'Zala']
+
+let selectedCountry = document.getElementById('country');
+//console.log(selectedCountry);
+//console.log(selectedCountry.value);
+let stateSelector = document.getElementById('state');
+
+function changeCountry() {
+    let selectedArray = (selectedCountry.value == 'hungary') ? hunCounties : usStates;
+    stateSelector.innerHTML = '';
+
+    selectedArray.map(item => {
+        stateSelector.insertAdjacentHTML('beforeend', `<option value="${item}">${item}</option>`);
+        });
+}
